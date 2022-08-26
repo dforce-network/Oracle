7026807594630747d87626fc5312129ae1fdf5e9
l@dforce.network <liujun.leo.lll@gmail.com>
2022-08-26 21:04:51 +0800

    update contracts: update readyToUpdate cancel anchor check

--- contracts/priceModel/PosterModel.sol
+++ contracts/priceModel/PosterModel.sol
@@ -513,19 +513,11 @@ contract PosterModel is Base, Unit {
     ) public view virtual returns (bool _success) {
         _postBuffer;
         uint256 _price;
-        uint256 _currentPeriod;
-        uint256 _anchorPeriod;
-        (_success, _price, _currentPeriod, _anchorPeriod, , ) = _updatePriceRes(
-            _asset,
-            _requestedPrice
-        );
+        (_success, _price, , , , ) = _updatePriceRes(_asset, _requestedPrice);
         if (_success) {
             uint256 _assetPrice = assetPrices_[_asset];
             (, uint256 _swing) = _calculateSwing(_assetPrice, _requestedPrice);
-            _success =
-                _swing >= _postSwing ||
-                (_currentPeriod > _anchorPeriod &&
-                    _price != anchors_[_asset].price);
+            _success = _swing >= _postSwing && _price != _assetPrice;
         }
     }
 
