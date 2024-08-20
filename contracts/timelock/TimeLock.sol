e3d876c35d7c5049ea250ef53a7d91d5ccbf31a2
l@dforce.network <liujun.leo.lll@gmail.com>
2024-08-20 20:25:49 +0800

    remove compile warning

--- contracts/timelock/TimeLock.sol
+++ contracts/timelock/TimeLock.sol
@@ -24,7 +24,7 @@ contract Timelock is Ownable {
         callData = abi.encodePacked(bytes4(keccak256(bytes(signature))), data);
 
         // solium-disable-next-line security/no-call-value
-        (bool success, bytes memory returnData) = target.call.value(value)(callData);
+        (bool success, bytes memory returnData) = target.call{ value: value }(callData);
         require(success, "Timelock::executeTransaction: Transaction execution reverted.");
 
         return returnData;
